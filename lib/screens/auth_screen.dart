d5b3612aced874ad2cc7ef6ad77802b3dbaabff1
Om Chevli <omchevli@gmail.com>
2020-07-10 16:29:45 +0530

    Edited Auth Screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -63,7 +63,7 @@ class AuthScreen extends StatelessWidget {
                       child: Text(
                         'MyShop',
                         style: TextStyle(
-                          color: Theme.of(context).accentTextTheme.title.color,
+                          color: Theme.of(context).accentColor,
                           fontSize: 50,
                           fontFamily: 'Anton',
                           fontWeight: FontWeight.normal,
